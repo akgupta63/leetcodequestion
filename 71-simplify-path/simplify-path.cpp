ebe4893a5547673095cf11aa20be2ba4e662be1b
Aman Kumar Gupta <agupta11040@gmail.com>
2026-02-05 16:13:30 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (84.04%) - LeetSync

---
+++ 71-simplify-path/simplify-path.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> stack;
+        string temp = "";
+
+        for (int i = 0; i <= path.size(); i++) {
+            if (i == path.size() || path[i] == '/') {
+                if (temp == "" || temp == ".") {
+                }
+                else if (temp == "..") {
+                    if (!stack.empty()) stack.pop_back();
+                }
+                else {
+                    stack.push_back(temp);
+                }
+                temp = "";
+            }
+            else {
+                temp += path[i];
+            }
+        }
+
+        string result = "/";
+        for (int i = 0; i < stack.size(); i++) {
+            result += stack[i];
+            if (i != stack.size() - 1)
+                result += "/";
+        }
+
+        return result;
+    }
+};
